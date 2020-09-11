5fabf11b940410bc0d54075d1f466a5b9fab7780
Jordan396 <jordan.chyehong@gmail.com>
2020-09-11 08:55:13 -0400

    Working prometheus and grafana

--- configs/kustomize/config_map_data/postgres/10-init.sql
+++ configs/kustomize/config_map_data/postgres/10-init.sql
@@ -1,22 +1,24 @@
+CREATE SCHEMA api;
+
 -- Create standard tables
-CREATE TABLE IF NOT EXISTS villages (
+CREATE TABLE IF NOT EXISTS api.villages (
     villageID SERIAL PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     details TEXT NOT NULL
 );
-CREATE TABLE IF NOT EXISTS pilgrims (
+CREATE TABLE IF NOT EXISTS api.pilgrims (
     pilgrimID SERIAL PRIMARY KEY,
     email VARCHAR(30) NOT NULL,
     passwd CHAR(60) NOT NULL,
     villageID INTEGER,
     CONSTRAINT fk_village FOREIGN KEY(villageID) REFERENCES villages(villageID)
 );
-CREATE TABLE IF NOT EXISTS innkeepers (
+CREATE TABLE IF NOT EXISTS api.innkeepers (
     innkeeperID SERIAL PRIMARY KEY,
     email VARCHAR(30) NOT NULL,
     passwd CHAR(60) NOT NULL
 );
-CREATE TABLE IF NOT EXISTS projects (
+CREATE TABLE IF NOT EXISTS api.projects (
     projectID SERIAL PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     details TEXT NOT NULL,
@@ -24,14 +26,14 @@ CREATE TABLE IF NOT EXISTS projects (
     memory INTEGER NOT NULL,
     storage INTEGER NOT NULL
 );
-CREATE TABLE IF NOT EXISTS tickets (
+CREATE TABLE IF NOT EXISTS api.tickets (
     ticketID SERIAL PRIMARY KEY,
     email VARCHAR(30) NOT NULL,
     topic VARCHAR(30) NOT NULL,
     details TEXT NOT NULL,
     isOpen BOOLEAN NOT NULL
 );
-CREATE TABLE IF NOT EXISTS usage (
+CREATE TABLE IF NOT EXISTS api.usage (
     projectID INTEGER NOT NULL,
     pilgrimID INTEGER NOT NULL,
     startTime TIMESTAMPTZ NOT NULL,
